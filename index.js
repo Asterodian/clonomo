2989fd1865b0dd1e0244a5b98c1954d64278068c
Yoru Akio <yoruakio@proton.me>
2024-11-10 00:56:36 +0700

    feat(webpage): adding compresstion on express server, building new interface for dashboard

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
+const compression = require('compression');
+
+// Using compression middleware
+app.use(compression({
+    level: 9,
+    threshold: 0,
+    filter: (req, res) => {
+        if (req.headers['x-no-compression']) {
+            return false;
+        }
+        return compression.filter(req, res);
+    }
+}));
+app.set('trust proxy', 1);
 
 // Middleware for CORS
 app.use(function (req, res, next) {
@@ -27,25 +41,19 @@ app.use(function (req, res, next) {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Endpoint POST for '/player/login/dashboard'
-app.post('/player/login/dashboard', (req, res) => {
+app.all('/player/login/dashboard', (req, res) => {
     res.sendFile(__dirname + '/public/html/dashboard.html');
 });
 
 // Endpoint POST for '/player/growid/login/validate'
 app.post('/player/growid/login/validate', (req, res) => {
-    // Extracting data from the request body
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
-
-    // Clear the token
     res.send(
         `{"status":"success","message":"Account Validated.","token":"","url":"","accountType":"growtopia"}`
     );
 });
 
 // Endpoint POST for '/player/validate/close'
-app.post('/player/validate/close', function (req, res) {
+app.all('/player/validate/close', function (req, res) {
     res.send('<script>window.close();</script>');
 });
 
@@ -53,27 +61,6 @@ app.post('/player/validate/close', function (req, res) {
 app.get('/', function (req, res) {
     res.send('ClonePS 200 Connection');
 });
-
-// Endpoint GET for '/player/login/dashboard'
-app.get('/player/login/dashboard', (req, res) => {
-    res.sendFile(__dirname + '/public/html/dashboard.html');
-});
-
-// Endpoint GET for '/player/growid/login/validate'
-app.get('/player/growid/login/validate', (req, res) => {
-    // Responding to GET request with empty token
-    res.send(
-        `{"status":"success","message":"Account Validated.","token":"","url":"","accountType":"growtopia"}`
-    );
-});
-
-// Endpoint GET for '/player/validate/close'
-app.get('/player/validate/close', function (req, res) {
-    res.send('<script>window.close();</script>');
-});
-
-
-
 
 // Start the server on port 5000
 app.listen(5000, function () {
